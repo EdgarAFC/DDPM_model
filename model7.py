b1ad1c057b3b9bbe7d369259af4a9934317c689e
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-03-21 18:49:09 -0500

    changing files

--- model7.py
+++ model7.py
@@ -122,6 +122,125 @@ class UNETv13(nn.Module):
             x = self.upAttention[idx](x)
 
         return self.final_block(x)
+    
+# class UNETv14(nn.Module):
+#     def __init__(
+#             self, 
+#             in_channels=3, 
+#             out_channels=1, 
+#             features=[64, 128, 256, 512], 
+#             emb_dim = 256,
+#             residual = True,
+#             attention_heads = 4,
+#             attention_res = [256,512],
+#             group_norm = True,
+#         ):
+#         super(UNETv14, self).__init__()
+
+#         self.time_mlp = nn.Sequential(
+#             PositionalEncoding(features[0]),
+#             linear(features[0], emb_dim),
+#             SiLU(),   #commented efernandez 13.03.2024
+#             # nn.ReLU(),
+#             linear(emb_dim, emb_dim),
+#         )
+
+#         self.initial_conv = conv_nd(2, in_channels, features[0], 3, padding=1)
+        
+#         self.initial_block = ResBlock(
+#             channels = features[0],
+#             emb_channels = emb_dim,
+#             dropout = 0,
+#             out_channels = features[0],
+#             use_conv = False,
+#             use_scale_shift_norm = True,
+#             dims = 2,
+#             use_checkpoint = False,
+#             residual=residual,
+#             group_norm=group_norm
+#         )
+
+#         # Down part of UNET
+#         self.downBlocks = nn.ModuleList()
+#         self.downAttention = nn.ModuleList()
+#         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+#         for feature in features:
+#             self.downBlocks.append(
+#                 ResBlock(
+#                     channels = feature,
+#                     emb_channels = emb_dim,
+#                     dropout = 0,
+#                     out_channels = feature*2,
+#                     use_conv = False,
+#                     use_scale_shift_norm = True,
+#                     dims = 2,
+#                     use_checkpoint = False,
+#                     residual=residual,
+#                     group_norm=group_norm
+#                 ))
+#             if feature in attention_res:
+#                 self.downAttention.append(AttentionBlock(
+#                     feature*2, use_checkpoint=False, num_heads=attention_heads
+#                 ))
+#             else:
+#                 self.downAttention.append(nn.Identity())                
+
+
+#         # Up part of UNET
+#         self.upBlocks = nn.ModuleList()
+#         self.upConvs = nn.ModuleList()
+#         self.upAttention = nn.ModuleList()
+#         for feature in reversed(features):
+#             self.upConvs.append(
+#                 nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2))
+#             self.upBlocks.append(
+#                 ResBlock(
+#                     channels = feature * 2,
+#                     emb_channels = emb_dim,
+#                     dropout = 0,
+#                     out_channels = feature,
+#                     use_conv = False,
+#                     use_scale_shift_norm = True,
+#                     dims = 2,
+#                     use_checkpoint = False,
+#                     residual=residual,
+#                     group_norm=group_norm
+#                 ))
+#             if feature in attention_res:
+#                 self.upAttention.append(AttentionBlock(
+#                     feature, use_checkpoint=False, num_heads=attention_heads
+#                 ))
+#             else:
+#                 self.upAttention.append(nn.Identity())
+
+
+#         self.final_block = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+#     def forward(self, x, y, t):
+#         # x: IQ image
+#         # y: Noisy Bmode
+#         time_emb = self.time_mlp(t)
+
+#         x = torch.cat((x,y), dim=1)
+#         x = self.initial_block(self.initial_conv(x),time_emb)
+
+#         # Convolutional layers and max-pooling
+#         skip_connections = []
+#         for idx in range(len(self.downBlocks)):
+#             skip_connections.append(x)
+#             x = self.pool(x)
+#             x = self.downBlocks[idx](x, time_emb)
+#             x = self.downAttention[idx](x)
+
+#         # Convolutional layers and up-sampling
+#         skip_connections = skip_connections[::-1]  # Reversing list
+#         for idx in range(len(self.upBlocks)):
+#             x = self.upConvs[idx](x)  # UpConvolution
+#             concat_skip = torch.cat((skip_connections[idx], x), dim=1)
+#             x = self.upBlocks[idx](concat_skip, time_emb)  # Double convs
+#             x = self.upAttention[idx](x)
+
+#         return self.final_block(x)
 
 # PositionalEncoding Source： https://github.com/lmnt-com/wavegrad/blob/master/src/wavegrad/model.py
 class PositionalEncoding(nn.Module):
@@ -177,13 +296,13 @@ class ResBlock(nn.Module):
 
         self.in_layers = nn.Sequential(
             normalization(channels, group_norm),
-            # SiLU(),   #commented efernandez 13.03.2024
-            nn.ReLU(),
+            SiLU(),   #commented efernandez 13.03.2024
+            # nn.ReLU(),
             conv_nd(dims, channels, self.out_channels, 3, padding=1),
         )
         self.emb_layers = nn.Sequential(
-            # SiLU(),   #commented efernandez 13.03.2024
-            nn.ReLU(),
+            SiLU(),   #commented efernandez 13.03.2024
+            # nn.ReLU(),
             linear(
                 emb_channels,
                 2 * self.out_channels if use_scale_shift_norm else self.out_channels,
@@ -191,8 +310,8 @@ class ResBlock(nn.Module):
         )
         self.out_layers = nn.Sequential(
             normalization(self.out_channels, group_norm),
-            # SiLU(),   #commented efernandez 13.03.2024
-            nn.ReLU(),
+            SiLU(),   #commented efernandez 13.03.2024
+            # nn.ReLU(),
             nn.Dropout(p=dropout),
             zero_module(
                 conv_nd(dims, self.out_channels, self.out_channels, 3, padding=1)
@@ -285,8 +404,8 @@ class ResBlock2(nn.Module):
             # SiLU(),   #commented efernandez 13.03.2024
             conv_nd(dims, channels, self.out_channels, 3, padding=1),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            # SiLU()
+            # nn.ReLU(),
+            SiLU()
             
         )
         self.emb_layers = nn.Sequential(
@@ -304,8 +423,8 @@ class ResBlock2(nn.Module):
                 conv_nd(dims, self.out_channels, self.out_channels, 3, padding=1)
             ),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            # SiLU(),
+            # nn.ReLU(),
+            SiLU(),
             nn.Dropout(p=dropout),
             # zero_module(
             #     conv_nd(dims, self.out_channels, self.out_channels, 3, padding=1)
