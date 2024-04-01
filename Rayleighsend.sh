533bf2f0713af5ad026e0f14ec524ced2c7b38fe
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-04-01 10:55:03 -0500

    adding .sh

--- Rayleighsend.sh
+++ Rayleighsend.sh
@@ -2,9 +2,9 @@
 #SBATCH --gpus-per-node=1
 #SBATCh --nodes=1
 #SBATCH --partition=gamerpcs
-#SBATCH --nodelist=worker1
-#SBATCH --output="log_model.out"
+#SBATCH --nodelist=worker7
+#SBATCH --output="log_rayleigh.out"
 
 source /etc/profile.d/modules.sh
-module load edgar/1.0
+module load students_env/1.0
 srun python /mnt/nfs/efernandez/projects/DDPM_model/main_rayleigh.py
