6bb1448ecfeb76397fa91a10f899b2b51cb58ba3
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-04-10 14:58:06 -0500

    adding files for worker 1

--- GenSamp_worker1.sh
+++ GenSamp_worker1.sh
@@ -3,8 +3,8 @@
 #SBATCh --nodes=1
 #SBATCH --partition=gamerpcs
 #SBATCH --nodelist=worker1
-#SBATCH --output="log_model.out"
+#SBATCH --output="log_model_w1.out"
 
 source /etc/profile.d/modules.sh
 module load edgar/1.0
-srun python /mnt/nfs/efernandez/projects/DDPM_model/gen_samples_w1.py
+srun python /mnt/nfs/efernandez/projects/DDPM_model/gen_samples_1.py
