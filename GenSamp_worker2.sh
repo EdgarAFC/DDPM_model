e2452ac5fc88b8d298e9a598a848ed9635b7c538
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-04-11 17:04:33 -0500

    adding files for worker 2 y cola

---
+++ GenSamp_worker2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#SBATCH --gpus-per-node=1
+#SBATCh --nodes=1
+#SBATCH --partition=gamerpcs
+#SBATCH --nodelist=worker2
+#SBATCH --output="log_model_w2.out"
+
+source /etc/profile.d/modules.sh
+module load edgar/1.0
+srun python /mnt/nfs/efernandez/projects/DDPM_model/gen_samples_2.py
