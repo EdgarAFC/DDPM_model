07cf17d6a4a57e349b8964dbcc7eb89cb2bec425
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-04-12 10:08:50 -0500

    adding files for worker 2 -1

--- GenSamp_worker1_cola.sh
+++ GenSamp_worker1_cola.sh
@@ -6,5 +6,5 @@
 #SBATCH --output="log_model_w2.out"
 
 source /etc/profile.d/modules.sh
-module load edgar/1.0
+module load ifsr-advpertbeamf/1.0
 srun python /mnt/nfs/efernandez/projects/DDPM_model/gen_samples_1_cola.py
