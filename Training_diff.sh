fec190e16ecdbad8995b6b6a9b995068f705d6f3
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-04-23 17:13:00 -0500

    adding files to train with less steps

---
+++ Training_diff.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#SBATCH --gpus-per-node=1
+#SBATCh --nodes=1
+#SBATCH --partition=gamerpcs
+#SBATCH --nodelist=worker1
+#SBATCH --output="log_model_w1.out"
+
+source /etc/profile.d/modules.sh
+module load edgar/1.0
+srun python /mnt/nfs/efernandez/projects/DDPM_model/main.py
