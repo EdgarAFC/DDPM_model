0cbe06d04ef8e392238f9bef91a8802d15d9bd8e
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-06-13 22:53:49 -0500

    model with more angles

--- main.py
+++ main.py
@@ -116,8 +116,8 @@ class ONEPW_Dataset2(Dataset):
 
         neg8_image_name = os.path.join(self.train_neg8_img, self.neg8_images[idx])
         neg8_image = np.load(neg8_image_name)
-        neg8_image = torch.Tensor(neg8_image)
         print("SHAPE neg8", neg8_image.shape)
+        neg8_image = torch.Tensor(neg8_image)
         neg8_image = neg8_image.permute(2, 0, 1)
 
         zero_image_name = os.path.join(self.train_zero_img, self.zero_images[idx])
